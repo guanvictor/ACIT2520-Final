aaf275065ac9c6dfd4a948f14ebbe573c702fb22
guanvictor <guanvictor@hotmail.com>
2019-04-17 01:23:55 -0700

    Exam Files

---
+++ server.js
@@ -0,0 +1,33 @@
+const axios = require('axios');
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+const bodyParser = require('body-parser');
+
+
+const port = process.env.PORT || 8080;
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`);
+});
+
+
